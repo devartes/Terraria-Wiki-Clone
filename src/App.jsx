5c139a2656433db83eef61f35b51872ecb94bbef
devartes <anacavalcante731@gmail.com>
2022-05-08 22:24:22 -0300

    atualizando

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ function App() {
             <img src={require("./assets/img/Site-logo-overworld.webp")} alt="logo wiki.gg" />
           </a>
         </div>
-
         <div className="mainBlock">
           <div className="asideBlock">
             <aside className="sidebarNavigation">
@@ -27,13 +26,13 @@ function App() {
               <ul>
                 <li><a href="/" title="Visit the main page [Alt+Shift+z]">Main page</a></li>
                 <li><a href="/" title="A list of recent changes in the wiki [Alt+Shift+r]">Recent changes</a></li>
-                <li><a href="/">Random page</a></li>
+                <li><a href="/" title="Load a random page [Alt+Shift+x]">Random page</a></li>
                 <li><a href="/">Wiki projects</a></li>
                 <li><a href="/">Wiki rules</a></li>
                 <li><a href="/">Style guide</a></li>
                 <li><a href="/">Community noticeboard</a></li>
                 <li><a href="/">Admin noticeboard</a></li>
-                <li><a href="/">Help</a></li>
+                <li><a href="/" title="The place to find out">Help</a></li>
               </ul>
             </aside>
             <aside className="subsidebarNavigation">
@@ -67,36 +66,73 @@ function App() {
             <aside className="subsidebarNavigation">
               <h3 className="subtitle">Tools</h3>
               <ul>
-                <li><a href="/">What links here</a></li>
-                <li><a href="/">Related changes</a></li>
-                <li><a href="/">Special pages</a></li>
-                <li><a href="/">Printable version</a></li>
-                <li><a href="/">Permanent link</a></li>
-                <li><a href="/">Page information</a></li>
+                <li><a href="/" title="A list of all wiki pages that link here [Alt+Shift+j]">What links here</a></li>
+                <li><a href="/" title="Recent changes in pages linked from this page [Alt+Shift+k]">Related changes</a></li>
+                <li><a href="/" title="A list of all special pages [Alt+Shift+q]">Special pages</a></li>
+                <li><a href="/" title="Printable version of this page [Alt+Shift+p]">Printable version</a></li>
+                <li><a href="/" title="Permanent link to this revision of the page">Permanent link</a></li>
+                <li><a href="/" title="More information about this page">Page information</a></li>
                 <li><a href="/">Page values</a></li>
-                <li><a href="/">Cite this page</a></li>
+                <li><a href="/" title="Information on how to cite this page">Cite this page</a></li>
               </ul>
             </aside>
             <aside className="subsidebarNavigation">
               <h3 className="subtitle">Other languages</h3>
               <ul>
-                <li><a href="/">Italiano</a></li>
-                <li><a href="/">Random page</a></li>
-                <li><a href="/">Wiki projects</a></li>
-                <li><a href="/">Wiki rules</a></li>
-                <li><a href="/">Style guide</a></li>
-                <li><a href="/">Community noticeboard</a></li>
-                <li><a href="/">Admin noticeboard</a></li>
-                <li><a href="/">Help</a></li>
+                <li><a href="/" title="Terraria Wiki/it – italiano">Italiano</a></li>
+                <li><a href="/" title="Terraria Wiki/yue – 粵語">粵語</a></li>
+                <li><a href="/" title="Terraria Wiki – français">Français</a></li>
+                <li><a href="/" title="Terraria Wiki – 中文">中文</a></li>
+                <li><a href="/" title="Terraria Wiki – português">Português</a></li>
+                <li><a href="/" title="Terraria Wiki/sv – svenska">Svenska</a></li>
+                <li><a href="/" title="Terraria Wiki/Редактируемая копия – русский">Русский</a></li>
+                <li><a href="/" title="Terraria Wiki – 한국어">한국어</a></li>
+                <li><a href="/" title="Terraria Wiki – Deutsch">Deutsch</a></li>
+                <li><a href="/" title="Terraria Wiki/vi – Tiếng Việt">Tiếng Việt</a></li>
+                <li><a href="/" title="Terraria Wiki – magyar">Magyar</a></li>
+                <li><a href="/" title="Terraria Wiki/lv – latviešu">Latviešu</a></li>
+                <li><a href="/" title="Terraria Wiki/ja – 日本語">日本語</a></li>
+                <li><a href="/" title="Terraria Wiki/tr – Türkçe">Türkçe</a></li>
+                <li><a href="/" title="Terraria Wiki/th – ไทย">ไทย</a></li>
+                <li><a href="/" title="Terraria Wiki/id – Bahasa Indonesia">Bahasa Indonesia</a></li>
+                <li><a href="/" title="Terraria Wiki – polski">Polski</a></li>
+                <li><a href="/" title="Terraria Wiki – українська">Українська</a></li>
+                <li><a href="/" title="Terraria Wiki/nl – Nederlands">Nederlands</a></li>
+                <li><a href="/" title="Terraria Wiki/es – español">Español</a></li>
+                <li><a href="/" title="Terraria Wiki/hi – हिन्दी">हिन्दी</a></li>
+                <li><a href="/" title="Terraria Wiki/no – norsk">Norsk</a></li>
+                <li><a href="/" title="Terraria Wiki/fi – suomi">Suomi</a></li>
+                <li><a href="/" title="Terraria Wiki/lt – lietuvių">Lietuvių</a></li>
+                <li><a href="/" title="Terraria Wiki/cs – čeština">Čeština</a></li>
+                <li><a href="/" title="Terraria Wiki/el – Ελληνικά">Ελληνικά</a></li>
               </ul>
             </aside>
+            <div className="merch">
+              <a href="/"><img src={require("./assets/img/Merch.webp")} alt="merch" /></a>
+            </div>
           </div>
           <div className="mainBlockContainer">
-            teste
+            <div className="head">
+              <div className="headerRight">
+                <div className="headerToggle">
+                <span></span>
+                </div>
+              </div>
+              <div className="headerLeft">oi</div>
+            </div>
           </div>
 
         </div>
       </div>
+      <footer>
+        <div className="footerRight"><a href="/">
+          © wiki.gg
+        </a>
+          Copyright 2022 All Rights Reserved
+          </div>
+        <div className="footerMiddle"> <img src={require("./assets/img/wiki-footer.png")} alt="wiki" /></div>
+        <div className="footerLeft"><a href="/">Terms of Service</a>|<a href="/">Privacy Policy</a></div>
+      </footer>
     </div>
   );
 }
