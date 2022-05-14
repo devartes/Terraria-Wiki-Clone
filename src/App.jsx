93c3a03cef1864966fb44ab1360e930945611828
devartes <anacavalcante731@gmail.com>
2022-05-14 10:17:20 -0300

    atualizando version content

--- src/App.jsx
+++ src/App.jsx
@@ -147,32 +147,48 @@ function App() {
                       </span>
                     </div>
                     <div className="midia">
-                      <ul className="links">
-                        <span>Links:</span>
-                        <li><a href="/"><img src={require("./assets/img/16px-Terraria_icon.png")} alt="terraria icon" /></a></li>
-                        <li><a href="/"><img src={require("./assets/img/16px-Re-Logic_icon.png")} alt="re-logic icon" /></a></li>
-                        <li><a href="/"><img src={require("./assets/img/16px-Facebook.svg.png")} alt="facebook icon" /></a></li>
-                        <li><a href="/"><img src={require("./assets/img/16px-Twitter.svg.png")} alt="twitter icon" /></a></li>
-                        <li><a href="/"><img src={require("./assets/img/16px-Instagram.svg.png")} alt="instagram icon" /></a></li>
-                        <li><a href="/"><img src={require("./assets/img/16px-Discord.svg.png")} alt="discord icon" /></a></li>
-                        <li><a href="/"><img src={require("./assets/img/16px-Reddit.svg.png")} alt="reddit icon" /></a></li>
-                        <li><a href="/"><img src={require("./assets/img/16px-Twitch.svg.png")} alt="twitch icon" /></a></li>
-                        <li><a href="/"><img src={require("./assets/img/16px-TikTok.svg.png")} alt="tiktok icon" /></a></li>
-                      </ul>
-                      <ul className="buyTerraria">
-                        <span>Buy Terraria:</span>
-                        <li><a href="/"><img src={require("./assets/img/16px-Steam.svg.png")} alt="steam icon" /></a></li>
-                        <li><a href="/"><img src={require("./assets/img/16px-GOG.com.svg.png")} alt="gog.com icon" /></a></li>
-                        <li><a href="/"><img src={require("./assets/img/16px-PlayStation_Store.svg.png")} alt="playstation icon" /></a></li>
-                        <li><a href="/"><img src={require("./assets/img/16px-Xbox_360.svg.png")} alt="xbox icon" /></a></li>
-                        <li><a href="/"><img src={require("./assets/img/16px-Xbox_One.svg.png")} alt="xbox one icon" /></a></li>
-                        <li><a href="/"><img src={require("./assets/img/16px-Google_Play.svg.png")} alt="google play icon" /></a></li>
-                        <li><a href="/"><img src={require("./assets/img/16px-Amazon.svg.png")} alt="amazon icon" /></a></li>
-                        <li><a href="/"><img src={require("./assets/img/16px-Apple_App_Store.svg.png")} alt="apple icon" /></a></li>
-                        <li><a href="/"><img src={require("./assets/img/16px-Stadia.svg.png")} alt="stadia icon" /></a></li>
-                        <li><a href="/"><img src={require("./assets/img/16px-Microsoft_Store.svg.png")} alt="microsoft icon" /></a></li>
-                        <li><a href="/"><img src={require("./assets/img/16px-Nintendo_eShop.svg.png")} alt="nintendo eshop icon" /></a></li>
-                      </ul>
+                      <div className="midiaContent">
+                        <ul className="links">
+                          <span>Links:</span>
+                          <li><a href="/"><img src={require("./assets/img/16px-Terraria_icon.png")} alt="terraria icon" /></a></li>
+                          <li><a href="/"><img src={require("./assets/img/16px-Re-Logic_icon.png")} alt="re-logic icon" /></a></li>
+                          <li><a href="/"><img src={require("./assets/img/16px-Facebook.svg.png")} alt="facebook icon" /></a></li>
+                          <li><a href="/"><img src={require("./assets/img/16px-Twitter.svg.png")} alt="twitter icon" /></a></li>
+                          <li><a href="/"><img src={require("./assets/img/16px-Instagram.svg.png")} alt="instagram icon" /></a></li>
+                          <li><a href="/"><img src={require("./assets/img/16px-Discord.svg.png")} alt="discord icon" /></a></li>
+                          <li><a href="/"><img src={require("./assets/img/16px-Reddit.svg.png")} alt="reddit icon" /></a></li>
+                          <li><a href="/"><img src={require("./assets/img/16px-Twitch.svg.png")} alt="twitch icon" /></a></li>
+                          <li><a href="/"><img src={require("./assets/img/16px-TikTok.svg.png")} alt="tiktok icon" /></a></li>
+                        </ul>
+                        <ul className="buyTerraria">
+                          <span>Buy Terraria:</span>
+                          <li><a href="/"><img src={require("./assets/img/16px-Steam.svg.png")} alt="steam icon" /></a></li>
+                          <li><a href="/"><img src={require("./assets/img/16px-GOG.com.svg.png")} alt="gog.com icon" /></a></li>
+                          <li><a href="/"><img src={require("./assets/img/16px-PlayStation_Store.svg.png")} alt="playstation icon" /></a></li>
+                          <li><a href="/"><img src={require("./assets/img/16px-Xbox_360.svg.png")} alt="xbox icon" /></a></li>
+                          <li><a href="/"><img src={require("./assets/img/16px-Xbox_One.svg.png")} alt="xbox one icon" /></a></li>
+                          <li><a href="/"><img src={require("./assets/img/16px-Google_Play.svg.png")} alt="google play icon" /></a></li>
+                          <li><a href="/"><img src={require("./assets/img/16px-Amazon.svg.png")} alt="amazon icon" /></a></li>
+                          <li><a href="/"><img src={require("./assets/img/16px-Apple_App_Store.svg.png")} alt="apple icon" /></a></li>
+                          <li><a href="/"><img src={require("./assets/img/16px-Stadia.svg.png")} alt="stadia icon" /></a></li>
+                          <li><a href="/"><img src={require("./assets/img/16px-Microsoft_Store.svg.png")} alt="microsoft icon" /></a></li>
+                          <li><a href="/"><img src={require("./assets/img/16px-Nintendo_eShop.svg.png")} alt="nintendo eshop icon" /></a></li>
+                        </ul>
+                      </div>
+                      <div className="versionContent">
+                        <div className="desktopBlock">
+                          <ul className="desktopIcons">
+                            <li><a href="/"><img src={require("./assets/img/26px-Windows_10.svg.png")} alt="windows" /></a></li>
+                            <li><a href="/"><img className="colorInvert" src={require("./assets/img/26px-MacOS.svg.png")} alt="macos" /></a></li>
+                            <li><a href="/"><img src={require("./assets/img/22px-Linux.svg.png")} alt="linux" /></a></li>
+                          </ul>
+                          <ul className="desktopVersion">
+                            <li className="nameSite"><a href="/">History</a></li>
+                            <li><a href="/">History</a></li>
+                            <li className="smallText"><a href="/">History</a></li>
+                          </ul>
+                        </div>
+                      </div>
                     </div>
                   </div>
 
